a72a4be0c5ccf831d5cad45bf7d09691ffd86901
Elena Balleste Izuzquiza <eballeste@minsait.com>
2021-04-15 13:36:42 +0200

    change logo for nav link for gh-pages

--- src/UI/Logo/Logo.js
+++ src/UI/Logo/Logo.js
@@ -1,11 +1,12 @@
 import React from 'react';
+import { NavLink } from 'react-router-dom'
 import logoImage from '../../assests/images/rick-morty-logo.png'
 import classes from './Logo.css'
 
 const logo = (props) => (
-    <a href="/" className={classes.Logo}>
+    <NavLink to="/" exact className={classes.Logo}>
         <img src={logoImage} alt="RickAndMortyLogo"/>
-    </a>
+    </NavLink>
 );
 
 export default logo;
